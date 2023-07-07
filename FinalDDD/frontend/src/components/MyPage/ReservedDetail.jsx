0344bc5a58e5654193fbcd6dccbb70ed8df5832f
YeonJoo <yj.nessa@gmail.com>
2023-07-07 10:50:08 +0900

    내 예약확인 수정- 결제정보 넘김

--- FinalDDD/frontend/src/components/MyPage/ReservedDetail.jsx
+++ FinalDDD/frontend/src/components/MyPage/ReservedDetail.jsx
@@ -4,6 +4,7 @@ import MobileTicket from './MobileTicket';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import TicketModal from './TicketModal';
+import { Co2Sharp } from '@mui/icons-material';
 
 
 // ====== data 확인하기 =====
@@ -21,7 +22,7 @@ const Container = styled.div`
     margin-bottom: .8rem;
     box-shadow: 0 3px 3px rgba(0,0,0,0.2);
     cursor: pointer;
-    
+
 
     .showImage{
         display: flex;
@@ -30,7 +31,7 @@ const Container = styled.div`
         /* background-color: blue; */
         padding: auto;
         margin-left: 1.3rem;
-        
+
     }
     .showImage img{
         width: 8rem;
@@ -49,7 +50,7 @@ const Container = styled.div`
         width: 100%;
         height: 76%;
         align-items: stretch;
-        
+
         .leftBox{
         display: flex;
         flex-direction: column;
@@ -80,7 +81,7 @@ const Container = styled.div`
                 float: right;
             }
         }
-    }   
+    }
 `;
 
 
@@ -97,20 +98,20 @@ const ReservedDetail = ({exhibitionData, currentPageData}) => {
       console.log(showModal)
 
     };
-  
+
     const closeModal = () => {
 
       setShowModal(false);
       console.log(showModal)
     };
-  
+
     const openMobileTicket = () => {
         return resevationData && openModal &&  <TicketModal resevationData={resevationData} closeModal={closeModal} />;
 
 
     }
-    
-    // 모바일티켓으로 데이터 전송하기 
+
+    // 모바일티켓으로 데이터 전송하기
     const [resevationData, setResevationData] = useState('');
 
     const handleData = (index) => {
@@ -120,37 +121,47 @@ const ReservedDetail = ({exhibitionData, currentPageData}) => {
 
         console.log(resevationData);
 
-    }; 
+    };
     useEffect(()=>{
         openMobileTicket();
     }, [resevationData]);
 
-    console.log("티켓금액 : " + currentPageData?.paymentDTO?.paidPrice);
+    console.log("currentData : ", currentPageData );
+    console.log("넘어오는 exhibitionData  : " , exhibitionData);
+    //console.log("넘어오는 데이터 중 금액 : ", exhibitionData[0].paymentDTO.paidPrice);
+    //console.log("넘어오는 데이터 중 crurrentData 중 금액 : ", currentPageData[0].paymentDTO.paidPrice);
 
+
+    // 결제일시 날짜형식변경
+    function formatDate(dateString) {
+        const date = new Date(dateString);
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        return `${year}-${month}-${day}`;
+      }
     return (
         <>
             <div className='count'>총 {totalRecords} 건</div>
 
                 {currentPageData.map((e) => (
                         <Container
-                            key={e.exhibitNo}
+                            key={e.bookingId}
                             onClick={ () => {
                                 handleData(e.index)
-                            }}
-
-                        >
+                            }}>
                             <div className="showImage"><img src={e.imgUrl} alt='exhibition' /></div>
                             <div className='justfyTop'>
                                 <div className='leftBox'>
                                     <div><span>전시명</span> {e.exhibitName}</div>
                                     <div><span>전시관</span>{e.exhibitLocation}</div>
                                     <div className='flexRow'>
-                                        <div className='row2'><span>관람인원</span>{e.paymentCnt}</div>
+                                        <div className='row2'><span>관람인원</span>{e.paymentDTO.paymentCnt}</div>
                                         <div className='row2'><span>관람일시</span>{e.visitDate}</div>
                                     </div>
                                     <div className='flexRow'>
-                                        <div className='row2'><span>금액</span>{e.paidPrice}</div>
-                                        <div className='row2'><span>결제일시</span>{e.paymentDate}</div>
+                                        <div className='row2'><span>금액</span>{e.paymentDTO.paidPrice}</div>
+                                        <div className='row2'><span>결제일시</span>{formatDate(e.paymentDTO.paymentDate)}</div>
                                     </div>
                                 </div>
                             </div>
