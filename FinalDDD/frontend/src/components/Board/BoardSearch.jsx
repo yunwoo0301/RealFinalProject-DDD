21217bb912e73899ec65ce4eb1262b28a28db6a7
yunwoo0301 <browniris7@naver.com>
2023-07-15 20:20:38 +0900

    검색창 미디어쿼리 위치 조정 완료

--- FinalDDD/frontend/src/components/Board/BoardSearch.jsx
+++ FinalDDD/frontend/src/components/Board/BoardSearch.jsx
@@ -4,71 +4,93 @@ import { IoSearch} from "react-icons/io5";
 import DDDApi from "../../api/DDDApi";
 
 const SearchWrap = styled.div` // 검색창 활성화 안된 부분 일부 위치 수정
-        display: flex;
-        width: 12em; 
-        height: 30px; 
-        padding: 0 5px; 
-        border: solid 2px #ddd; 
-        background-color: white;
-        margin: .7em 1em;
-        margin-left: auto;
-        border-radius: 5px;
-        cursor: pointer;
-            
-        input {width: 160px; height: 26px; border: 0px; outline: none; margin-right: 10px;}
-        
-        .icon_container {
-           
-            .searchicon {
-                color: #ccc;
-                margin-top: 6px;
-           }
+
+    display: flex;
+    width: 12em;
+    height: 30px;
+    padding: 0 5px;
+    border: solid 2px #ddd;
+    background-color: white;
+    margin: .7em 1em;
+    margin-left: auto;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: border-color 0.3s ease; // 테두리 색상 변화에 부드러운 전환 효과
+
+    :hover {
+            border-color: black; // 호버 했을 때의 테두리 색상
+        }
+
+    @media (max-width: 768px) {
+        margin : 0 auto;
+        margin-left: 34em;
     }
-        
-        
+
+    input {
+        width: 160px;
+        height: 26px;
+        border: 0px;
+        outline: none;
+        margin-right: 10px;
+    }
+
+    .icon_container {
+
+
+        .searchicon {
+            color: #8c8787;
+            margin-top: 6px;
+        }
+    }
+
 `;
+
+
+
 const BoardSearch = ({ onSearch }) => {
 
      // 검색
-    const [keyword, setKeyword] = useState("");
-   
-    const onChangeSearchKeyword = (e) => {
-        setKeyword(e.target.value);
-    };
+     const [keyword, setKeyword] = useState("");
 
-    const onClickSearch = async () => {
-        try {
+     const onChangeSearchKeyword = (e) => {
+         setKeyword(e.target.value);
+     };
+
+     const onClickSearch = async () => {
+         try {
             const response = await DDDApi.searchListLoad(keyword);
             const boardList = response.data;
+            console.log("넘어오는 데이터값 확인 :" + response.data);
             onSearch(boardList); // 검색 결과를 부모 컴포넌트로 전달
             } catch (e) {
             console.log(e);
             }
-    };
+     };
 
-    // 엔터를 눌렀을 때도 검색 되게
-    const onKeyEnterSearch = (e) => {
-        if (e.key === "Enter") {
-          onClickSearch();
-          setKeyword(""); // 검색 후 검색창 빈칸으로 만들기
-        }
-      };
+     // 엔터를 눌렀을 때도 검색 되게
+     const onKeyEnterSearch = (e) => {
+         if (e.key === "Enter") {
+           onClickSearch();
+           setKeyword(""); // 검색 후 검색창 빈칸으로 만들기
+         }
+       };
 
- 
 
-    return(
-        <SearchWrap>
-            <input name="searchkeyword" 
-            title="검색" 
-            placeholder="검색어를 입력하세요" 
-            onChange={onChangeSearchKeyword} 
-            onKeyDown={onKeyEnterSearch}
-            value={keyword}/>
+
+     return(
+         <SearchWrap>
+            <input
+                name="searchkeyword"
+                title="검색"
+                placeholder="검색어를 입력하세요"
+                onChange={onChangeSearchKeyword}
+                onKeyDown={onKeyEnterSearch}
+                value={keyword}/>
             <div className="icon_container">
                 <IoSearch className="searchicon" onClick={onClickSearch}/>
-            </div>
-        </SearchWrap> 
-    )
-};
+             </div>
+         </SearchWrap>
+     )
+ };
 
-export default BoardSearch;
+ export default BoardSearch;
