7849df7ff974967e908c1d749a608a64165e7796
YeonJoo <yj.nessa@gmail.com>
2023-07-16 00:44:12 +0900

    모바일티켓 - 미디어쿼리 적용

--- FinalDDD/frontend/src/components/MyPage/MobileTicket.jsx
+++ FinalDDD/frontend/src/components/MyPage/MobileTicket.jsx
@@ -1,139 +1,102 @@
-import React from 'react';
-import styled from 'styled-components';
-import CancelTicket from './CancelTicket';
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import dayjs from 'dayjs';
-import TicketBarcode from '../reservation/Barcode';
+import React, { useState } from "react";
+import styled from "styled-components";
+import  Button from "../../util/Button";
+import { ImHome } from "react-icons/im";
+import MobileTicket from "../MyPage/MobileTicket";
+import dayjs from "dayjs";
+import { useNavigate } from "react-router-dom";
 
 
-const Modal = styled.div`
-    width: 20vw;
-    height: 80vh;
-    min-width: 500px;
-    min-height: 700px;
-    /* height: 70vh; */
-    background-color: white;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate3d(-50%, -50%, 0);
-    border-radius: 1.4rem;
+const FinalContainer = styled.div`
+  .body{
     display: flex;
     flex-direction: column;
-    align-items: center;
-    border: 1px solid #ccc;
-    z-index: 3;
-
-    .closeBtn{
-        /* color: white; */
-        font-size: 1.4rem;
-        /* border-radius: 1rem; */
-        width: 2rem;
-        height: 4rem;
-        /* padding-bottom: .2rem; */
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: relative;
-        /* left: 44%; */
-        top: 0;
-        cursor: pointer;
+  }
+  @media (max-width: 768px) {
+    .root{
+      font-size: 0.8rem;
     }
+  }
 
-    .title{
-        font-size: 2rem;
-        /* background-color: red; */
-        padding-bottom: 0.5rem;
-    }
-    .exhImg{
-        width: 50%;
-        height: 20rem;
-        /* background-color: aqua; */
-        img{
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-            object-position: top;
-            border: 1px solid #ccc;
-        }
-    }
-    .desc{
-        text-align: center;
-        padding: 0.5rem;
-        span{
-            line-height: 1.8rem;
-        }
-        p{  
-            margin-top: 0;
-            font-size: 0.5rem;
-            color: red;
-        }
-
-    }
-    .btnBlock{
-            width: 90%;
-            display: flex;
-            justify-content: center;
-
-            button{
-                width: 22%;
-                height: 2rem;
-                border-radius: 3rem;
-                margin : 0rem .5rem;
-                background-color: #5EADF7 ;
-                border: 1px solid #F4F8FF;
-                color: white;
-                font-size: .8rem;
-                cursor: pointer;
-                /* float : right; */
-                display: flex;
-                justify-content:center;
-                align-items: center;
-                .link{
-                    text-decoration: none;
-                    color: inherit;
-                }
-            }
-        }
 `;
 
 
-const MobileTicket = ({ reservationData, closeModal, openCancel }) => {
-    // 날짜를 가지고와서 yy년 mm월 dd일로 표시하기위해
-    const formatSelectedDate = (date) => {
-        if (date) {
-        return dayjs(date).format('YYYY년 MM월 DD일');
-        }
-        return '';
-    };
+const FinalReservation = ({props}) => {
+    const navigate = useNavigate();
+    const getId = window.localStorage.getItem("memberId");
 
-    return (
-        <Modal>
-            <div className='closeBtn' onClick={closeModal}>&times;</div>
+    //결제 후에는 예약하기, 예약정보, 결제정보확인을 못가게하기위한 alert창띄우기
+    const showAlert = (message) => {
+        alert(message);
+      };
 
-            <div className='title'>TICKET</div>
-            <div className='exhImg'>
-                <img src={reservationData.imgUrl} alt='exhibition' />
+    // 모바일티켓 및 현장발권용 티켓출력
+    const [showTicket, setShowTicket] = useState(false);
+
+    const clickToTicket =() =>{
+        setShowTicket(true);
+    }
+
+    const closeTicket = () => {
+        setShowTicket(false);
+    }
+
+    // YYMMDD 형태로 전달
+    const visitDateDigits = dayjs(props.reservationData).format('YYMMDD');
+
+    // Ticket에 예매정보전달
+    const rData = {
+        imgUrl: props.imgUrl,
+        name: props.exhibitName,
+        place: props.exhibitLocation,
+        visitDate: props.reservationDate,
+        index: props.id,
+        deliveryMethod: props.deliveryMethod,
+        barcodeNo: visitDateDigits
+    }
+
+    const openMyBooking = () => {
+      navigate(`/mypage/${getId}`);
+    }
+
+    return(
+        <>
+        <FinalContainer imgUrl ={props.imgUrl}>
+            <div className="reservationBox">
+                <div className="root">
+                <ImHome/><p onClick={props.handleGoToHome}>전시 상세정보 페이지</p>
+                <p onClick={() => showAlert('예매완료 후 에는 해당페이지로 이동이 불가합니다.')}>{props.rootData[0]}</p>
+                <p onClick={() => showAlert('예매완료 후 에는 해당페이지로 이동이 불가합니다.')}>{props.rootData[1]}</p>
+                <p onClick={() => showAlert('예매완료 후 에는 해당페이지로 이동이 불가합니다.')}>{props.rootData[2]}</p>
+                <p>{props.rootData[3]}</p>
+                </div>
+                <div className="body">
+              <div className="infoBox">
+              <div className="imgBox"/>
+                <div className="textBox">
+                    <div className="title">{props.exhibitName}</div>
+                    <div>{props.exhibitLocation}</div>
+                </div>
+              </div>
+            <div className="rightBox">
+              <div className="btnContainer">
+              {props.deliveryMethod === "mobileTicket" && <Button className="btn" onClick={clickToTicket}>모바일티켓</Button>}
+              {props.deliveryMethod === "onSite" && <Button className="btn" onClick={clickToTicket}>현장발권용티켓</Button>}
+              <Button className="btn" onClick={openMyBooking}>예매확인</Button>
+              </div>
+              </div>
+              </div>
             </div>
-            <div className="desc">
-                <span style={{ fontWeight: 'bold' }}> {reservationData.name}</span><br />
-                <span> {reservationData.place}</span><br />
-                <span>관람일 : </span>
-                <span> {reservationData.visitDate && formatSelectedDate(reservationData.visitDate) }</span><br />
-                {/* 바코드 출력 */}
-                <span><TicketBarcode visitDate = {reservationData.barcodeNo} id ={reservationData.index}/></span>
-                {/* 현장 발권일 때는 주의 문구 출력 */}
-                {reservationData.deliveryMethod === "onSite" && <p>* 현장 발권일 경우, 관람 전 실물 티켓으로 교환 후 관람 가능합니다.</p>}<br />
-            </div>
-{/*             <div className='btnBlock'> */}
-{/*                 <button> */}
-{/*                     <Link to={`/exhibitInfo/${reservationData.index}`} className='link'>상세정보</Link></button> */}
-{/*                 <button onClick={openCancel}>예매취소</button> */}
-{/*             </div> */}
-        </Modal>
+        </FinalContainer>
+        {showTicket && (
+        <MobileTicket
+          reservationData={rData}
+          closeModal={closeTicket}
+        />
+        )}
+        </>
+    )
 
-    );
-};
+}
 
-export default MobileTicket;
+export default FinalReservation;
