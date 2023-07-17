e1e127b706bc13bb2bd79043b2fa571890c4a5e4
yunwoo0301 <browniris7@naver.com>
2023-07-17 16:09:50 +0900

    동행찾기 게시판 - 미디어쿼리 수정

--- FinalDDD/frontend/src/components/Board/Card.jsx
+++ FinalDDD/frontend/src/components/Board/Card.jsx
@@ -8,11 +8,23 @@ import PageNation from "../../util/PageNation";
 import DDDApi from "../../api/DDDApi";
 import { Link } from "react-router-dom";
 
+const Wrapper = styled.div` // 동행찾기 게시판 전체 컨테이너 영역
+    width: 100vw;
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
 
-const CardContainer = styled.div`
-    max-width: 100vw;
+    @media(max-width:768px) {
+        width: 100%;
+    }
+
+`;
+
+const CardContainer = styled.div` // 전체 카드아이템 컨테이너
+    max-width: 80em;
     display: grid;
-    grid-template-columns: repeat(auto-fit, 22.2em); // 반복(자동맞춤, 1fr 크기)  카드 이미지 추후 변경 예정
+    grid-template-columns: repeat(4, 1fr); // 반복(자동맞춤, 1fr 크기)  카드 이미지 추후 변경 예정
     justify-content: center;
     margin: 2rem;
     gap: 1rem;
@@ -25,7 +37,7 @@ const CardContainer = styled.div`
         margin: 0;
         padding:0;
     }
-    .container {
+    .container { // 카드아이템 컨테이너
         overflow: hidden; /* 영역을 벗어날 시 가려짐 */
         box-shadow: 0 1px 2px 0px;
         background-color: #ffffff;
@@ -34,16 +46,30 @@ const CardContainer = styled.div`
         position: relative;
         margin: 0.8rem;
         cursor: pointer;
+        height: auto; // 내용물에 따라 높이 자동 조절
+
     }
 
     .img_area {
+        width: 100%;
+        height: 15rem;
+        position:relative;
+        top:0;
+
+
     .cardimage {
         width: 100%;
-        height: 11rem;
+        height: 100%;
+        object-fit: cover;
+
+
     }
 }
 
-    .region, .cardtitle, .cardnickname, .datearea {
+    .cardinfo { // 카드정보 영역(지역, 제목, 작성자, 작성일)
+        height: 8rem;
+
+        .region, .cardtitle, .cardnickname, .datearea {
         text-align: left;
         margin-left: 1rem;
         margin: .4rem;
@@ -52,13 +78,14 @@ const CardContainer = styled.div`
     }
 
     .viewarea, .commentarea {
-        margin-left: .2em; // 아이콘과 숫자 사이 여백
+        margin-left: .2em; // 아이콘과 숫자 사이 여백 */
         margin-top: .2em; // 아이콘 옆 숫자 위치
         font-size: 1em;
-
     }
 
-    .icon-container {
+}
+
+    .icon-container { /* 조회수 댓글 영역 */
         display: flex;
         justify-content: flex-end;
         margin-right: 1.5rem;
@@ -68,41 +95,63 @@ const CardContainer = styled.div`
         font-size: 1.6rem;
         }
     }
-    .writebtn {
-        display: flex;
-        margin-bottom: 1em;
 
-        button {
-            margin: -1em 1em ;
-            margin-left: auto;
-            font-size: .9em;
-            padding: .5em 2em;
-            border-radius: 10px;
-            background-color: #050E3D;
-            color: white;
-            border: none;
-            transition: all .1s ease-in;
-            cursor: pointer;
-            font-weight: bold;
-        }
+    @media(max-width:768px) {
+        width: 100%;
+        grid-template-columns: repeat(2, 1fr);
     }
 `;
 
+
+const SelectWrapper = styled.div`
+    width: 80em;
+
+    @media (max-width: 768px) {
+        width: 100%;
+        float: right;
+    }
+
+`;
+
 const SelectBox = styled.select`
-    width: 100px;
-    height: 30px;
-    float: right;
+    width: 6rem;
+    height: 2rem;
     display: flex;
-    margin-right: 17em;
+    float: right;
+
     option{
         font-size: 20px;
     }
 
     @media (max-width: 768px) {
-        margin-left: 10em;
     }
  `;
 
+const WriteArea = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+
+    .writebtn {
+        height: 2rem;
+
+
+    }
+
+    button {
+        width: 6rem;
+        height: 2rem;
+        display: flex;
+        margin-right: 22em;
+    }
+
+    @media(max-width:768px) {
+        button {
+        margin-right: 2em;
+    }
+}
+
+`;
 
 
 const Card = () => {
@@ -186,31 +235,38 @@ const Card = () => {
 
     return(
         <>
-        <SelectBox value={selectedRegion} onChange={handleRegionChange}>
-            <option value="">전체</option>
-            <option value="서울">서울</option>
-            <option value="경기">경기</option>
-            <option value="인천">인천</option>
-            <option value="충청">충청</option>
-            <option value="강원">강원</option>
-            <option value="경상도">경상도</option>
-            <option value="전라도">전라도</option>
-            <option value="제주">제주</option>
-        </SelectBox>
+
+        <Wrapper>
+        <SelectWrapper>
+            <SelectBox value={selectedRegion} onChange={handleRegionChange}>
+                <option value="">전체</option>
+                <option value="서울">서울</option>
+                <option value="경기">경기</option>
+                <option value="인천">인천</option>
+                <option value="충청">충청</option>
+                <option value="강원">강원</option>
+                <option value="경상도">경상도</option>
+                <option value="전라도">전라도</option>
+                <option value="제주">제주</option>
+            </SelectBox>
+        </SelectWrapper>
         <CardContainer>
             {currentPageData.map((data, index) => (
                 <div className="container" key={index}>
                 <Link to={`/boardList/boardView/${data.boardNo}`} className="boardView_link" style={{ textDecoration: 'none', color: 'inherit' }}>
+
                 <div className="img_area">
                     <img src={data.image} alt="CardImage" className="cardimage" />
                 </div>
+                <div className = "cardinfo">
+                    <div className="region">
+                        <IoMdPin style={{fontSize:'1.2em', color:'#528BF9'}}/>{data.region}
+                    </div>
 
-                <div className="region">
-                    <IoMdPin style={{fontSize:'1.2em', color:'#528BF9'}}/>{data.region}
+                    <h3 className="cardtitle">{data.title}</h3>
+                    <div className="cardnickname">{data.author}</div>
+                    <div className="datearea">{data.writeDate.substring(0, 10)}</div>
                 </div>
-                <h3 className="cardtitle">{data.title}</h3>
-                <div className="cardnickname">{data.author}</div>
-                <div className="datearea">{data.writeDate.substring(0, 10)}</div>
 
                 <div className="icon-container">
                     <IoMdEye className="icon" style={{color:'#686565'}} />
@@ -220,13 +276,16 @@ const Card = () => {
                 </div>
                 </Link>
             </div>
-
         ))}
       </CardContainer>
+      </Wrapper>
        <PageNation pageCount={pageCount} onPageChange={handlePageClick} />
-       <div className="writebtn">
-            <button onClick={onClickToWrite}>글쓰기</button>
-        </div>
+       <WriteArea>
+            <div className="writebtn">
+                <button onClick={onClickToWrite}>글쓰기</button>
+            </div>
+        </WriteArea>
+        {/* </Wrapper> */}
       </>
     )
 };
